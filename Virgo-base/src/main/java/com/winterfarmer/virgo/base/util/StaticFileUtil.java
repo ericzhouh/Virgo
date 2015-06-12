d641da48a6c91b88aa3e195343a9cffc342c69f9
yangtianhang <yangtianhang@gmail.com>
2015-06-12 15:08:42 +0800

    up

--- Virgo-base/src/main/java/com/winterfarmer/virgo/base/util/StaticFileUtil.java
+++ Virgo-base/src/main/java/com/winterfarmer/virgo/base/util/StaticFileUtil.java
@@ -1,5 +1,7 @@
 package com.winterfarmer.virgo.base.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by yangtianhang on 15/6/4.
  */
@@ -7,6 +9,10 @@ public class StaticFileUtil {
     private static final String BASE_PORTRAIT_URL = "http://7xj66j.com1.z0.glb.clouddn.com/";
 
     public static String getPortraitUrl(String imageId) {
-        return BASE_PORTRAIT_URL + imageId;
+        if (StringUtils.isEmpty(imageId)) {
+            return null;
+        } else {
+            return BASE_PORTRAIT_URL + imageId;
+        }
     }
 }
